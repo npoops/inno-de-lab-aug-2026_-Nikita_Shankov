b8896a0c9c72d65638306a97855134b7f2edb67d
npoops <kopiavseh@gmail.com>
2026-08-13 11:53:10 +0300

    Update where_task1.sql

--- lesson3_sql_homework/where_task1.sql
+++ lesson3_sql_homework/where_task1.sql
@@ -1,7 +1,10 @@
+-- фильтруем по стране и возрасту.
 SELECT 
 	first_name, last_name, age, country
 FROM 
 	customers
 WHERE 
+	-- Оставляем только пользователей из Америки
 	country = 'USA'
+	-- И отсекаем всех, кому 25 или меньше
 	AND age > 25;
