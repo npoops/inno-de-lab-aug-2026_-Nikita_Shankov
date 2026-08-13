83fe1b535e0da980f84f3786da4af3877f8b9b62
npoops <kopiavseh@gmail.com>
2026-08-13 11:51:15 +0300

    Update subqueries_task1.sql

--- lesson3_sql_homework/subqueries_task1.sql
+++ lesson3_sql_homework/subqueries_task1.sql
@@ -1,13 +1,17 @@
+-- Вытягиваем имя, фамилию и сумму из чека с максималкой
 SELECT
 	c.first_name,
 	c.last_name,
 	o.amount
 FROM
 	orders o
+-- Привязываем покупателей к заказам, чтобы узнать, чей это чек
 INNER JOIN
 	customers c 
 	ON o.customer_id = c.customer_id 
 WHERE 
+	-- Подзапросом находим максимальный чек по всей таблице orders
+	-- и оставляем только те заказы, где сумма совпала с этим максимумом
 	o.amount =(
 		SELECT MAX(amount)
 		FROM orders
