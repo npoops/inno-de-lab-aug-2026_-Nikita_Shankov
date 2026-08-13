0d88922d44c2214bf340d6c38f1fd048d95bb910
npoops <kopiavseh@gmail.com>
2026-08-13 11:56:57 +0300

    Update window_task1.sql

--- lesson3_sql_homework/window_task1.sql
+++ lesson3_sql_homework/window_task1.sql
@@ -1,10 +1,14 @@
+-- через оконку закидываем итоговую сумму прямо в каждую строчку с заказом
 SELECT
 	order_id,
 	customer_id,
 	item,
 	amount,
+	-- Оконка: считает общую сумму всех заказов конкретного customer_id,
+	-- не схлопывая при этом сами строки (в отличие от обычного GROUP BY)
 	SUM(amount) OVER(PARTITION BY customer_id) AS total_by_customer
 FROM
 	orders
+-- Сортируем результат, чтобы заказы одного и того же клиента шли подряд
 ORDER BY 
 	customer_id, order_id;
