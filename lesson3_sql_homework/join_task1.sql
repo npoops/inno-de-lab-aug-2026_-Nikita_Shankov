de0cf9dc9ee58f42f4ed86d5a6db0bd4cea529bd
npoops <kopiavseh@gmail.com>
2026-08-13 11:46:02 +0300

    Update join_task1.sql

--- lesson3_sql_homework/join_task1.sql
+++ lesson3_sql_homework/join_task1.sql
@@ -1,3 +1,4 @@
+-- Собираем покупателей вместе с их заказами в одну таблицу.
 SELECT
 	c.first_name,
 	c.last_name, 
@@ -5,7 +6,9 @@ SELECT
 	o.amount 
 FROM
 	orders o
+-- Соединяем заказы с клиентами по их ID.
+-- Inner join оставляет только те записи, где есть совпадение (если у клиента нет заказов — он не попадет)
 INNER JOIN 
 	customers c 
 	ON o.customer_id = c.customer_id;
-	
+
